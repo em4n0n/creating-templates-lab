866f5ceec2fec099c538e7f03e491ed0309f939e
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-09-22 15:09:18 -0700

    Add a declaration for the list

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -119,5 +119,7 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
+STATICFILES_DIRS = [
+    'myapp/static',
+]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
